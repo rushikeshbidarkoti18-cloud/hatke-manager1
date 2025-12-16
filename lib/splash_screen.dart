d841c9acbbbcff8bc69eee2d8bf9d8b09fd02c62
rushikeshbidarkoti18-cloud <rushikeshbidarkoti.18@gmail.com>
2025-12-16 22:09:07 +0530

    Create splash_screen.dart

---
+++ lib/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+  late Animation<double> _scale;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _scale = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutBack,
+    );
+
+    _controller.forward();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      body: Center(
+        child: ScaleTransition(
+          scale: _scale,
+          child: Image.asset(
+            'assets/images/logo.png',
+            width: 280,
+          ),
+        ),
+      ),
+    );
+  }
+}
